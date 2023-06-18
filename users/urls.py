6dd7a5afd1b428f5b29dadf4c6a9490eca5d8728
dalienst <oduordalienst@gmail.com>
2023-06-18 19:57:59 +0300

    fix student and teacher profile

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 
-from users.views import TeacherSignUpView, dashboard, SignUpView, StudentSignUpView, SupervisorSignUpView
+from users.views import (
+    TeacherSignUpView,
+    dashboard,
+    SignUpView,
+    StudentSignUpView,
+    SupervisorSignUpView,
+    UserDetailView,
+    UserListView,
+    TeacherDetailView,
+    TeacherDeleteView,
+    TeacherUpdateView,
+    StudentDeleteView,
+    StudentDetailView,
+    StudentUpdateView,
+)
 
 app_name = "users"
 
@@ -11,5 +25,25 @@ urlpatterns = [
     path("signup/student/", StudentSignUpView.as_view(), name="student-signup"),
     path(
         "signup/supervisor/", SupervisorSignUpView.as_view(), name="supervisor-signup"
+    ),
+    path("users/", UserListView.as_view(), name="users"),
+    path("user/<str:pk>/detail/", UserDetailView.as_view(), name="user-detail"),
+    path(
+        "profile/<str:pk>/student/", StudentDetailView.as_view(), name="student-profile"
+    ),
+    path(
+        "update/<str:pk>/student/", StudentUpdateView.as_view(), name="student-update"
+    ),
+    path(
+        "delete/<str:pk>/student/", StudentDeleteView.as_view(), name="student-delete"
+    ),
+    path(
+        "profile/<str:pk>/teacher/", TeacherDetailView.as_view(), name="teacher-profile"
+    ),
+    path(
+        "update/<str:pk>/teacher/", TeacherUpdateView.as_view(), name="teacher-update"
+    ),
+    path(
+        "delete/<str:pk>/teacher/", TeacherDeleteView.as_view(), name="teacher-delete"
     ),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ListView,
+    DetailView,
+)
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
+from django.urls import reverse_lazy
 
 
 from users.forms import TeacherCreationForm, StudentCreationForm, SupervisorCreationForm
@@ -15,6 +26,26 @@ from classroom.settings import AUTH_USER_MODEL
 @login_required
 def dashboard(request):
     return render(request, "users/dashboard.html")
+
+
+class UserListView(UserPassesTestMixin, ListView, LoginRequiredMixin):
+    """admin sees all users"""
+
+    model = AUTH_USER_MODEL
+    template_name = "users/users_list.html"
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+class UserDetailView(UserPassesTestMixin, DetailView):
+    """details on specific users"""
+
+    model = AUTH_USER_MODEL
+    template_name = "users/user_detail.html"
+
+    def test_func(self):
+        return self.request.user.is_staff
 
 
 class SignUpView(TemplateView):
@@ -37,6 +68,47 @@ class TeacherSignUpView(SuccessMessageMixin, CreateView):
         return redirect("users:dashboard")
 
 
+class TeacherDetailView(LoginRequiredMixin, DetailView):
+    """Teacher views their details"""
+
+    model = Teacher
+    fields = [
+        "image",
+        "phonenumber",
+        "about",
+        "qualifications",
+    ]
+    template_name = "users/teacher_profile.html"
+
+
+class TeacherUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    """Teacher updates profile"""
+
+    model = Teacher
+    fields = [
+        "image",
+        "phonenumber",
+        "about",
+        "qualifications",
+    ]
+    template_name = "users/teacher_update.html"
+    success_url = reverse_lazy("users:dashboard")
+
+    def test_func(self):
+        return self.request.user.is_staff
+
+
+class TeacherDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    """Teacher deletes their profile"""
+
+    model = AUTH_USER_MODEL
+    template_name = "users/teacher_delete.html"
+    success_url = reverse_lazy("login")
+
+    def test_func(self):
+        return self.request.user.is_teacher
+
+
 class StudentSignUpView(SuccessMessageMixin, CreateView):
     model = AUTH_USER_MODEL
     form_class = StudentCreationForm
@@ -51,6 +123,39 @@ class StudentSignUpView(SuccessMessageMixin, CreateView):
         user = form.save()
         login(self.request, user)
         return redirect("users:dashboard")
+
+
+class StudentDetailView(LoginRequiredMixin, DetailView):
+    """View the details of the student"""
+
+    model = Student
+    fields = [
+        "image",
+        "phonenumber",
+        "about",
+    ]
+    template_name = "users/student_profile.html"
+
+
+class StudentUpdateView(LoginRequiredMixin, UpdateView):
+    """student updates their profile"""
+
+    model = Student
+    fields = [
+        "image",
+        "phonenumber",
+        "about",
+    ]
+    template_name = "users/student_update.html"
+    success_url = reverse_lazy("users:dashboard")
+
+
+class StudentDeleteView(LoginRequiredMixin, DeleteView):
+    """student deletes their profile"""
+
+    model = AUTH_USER_MODEL
+    template_name = "users/student_delete.html"
+    success_url = reverse_lazy("login")
 
 
 class SupervisorSignUpView(SuccessMessageMixin, CreateView):
