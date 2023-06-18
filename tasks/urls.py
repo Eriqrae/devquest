06b32aec9be5d11cb4de2998e4c4e648f9bae667
dalienst <oduordalienst@gmail.com>
2023-06-19 01:39:58 +0300

    create tasks app

---
+++ tasks/urls.py
@@ -0,0 +1,39 @@
+from django.urls import path
+from tasks.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskDetailView,
+    TaskSubmissionCreateView,
+    TaskSubmissionApproveView,
+    TaskSubmissionListView,
+    task_submission_create,
+    task_list,
+)
+
+app_name = "tasks"
+
+urlpatterns = [
+    path("tasks/", task_list, name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<str:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path(
+        "tasks/submissions/list/",
+        TaskSubmissionListView.as_view(),
+        name="task-submission-list",
+    ),
+    path(
+        "tasks/<str:task_id>/submit/",
+        task_submission_create,
+        name="task-submission-create",
+    ),
+    path(
+        "activity/tasks/submissions/<str:pk>/approve/",
+        TaskSubmissionApproveView.as_view(),
+        name="task-submission-approve",
+    ),
+    path(
+        "activity/tasks/submissions/<str:pk>/disapprove/",
+        TaskSubmissionApproveView.as_view(),
+        name="task-submission-disapprove",
+    ),
+]
